d3ffd25d425c7a01055661d5f72d20c1ed1a3b68
NINJA88423 <cesar.smiguel89@gmail.com>
2026-03-09 21:28:07 -0300

    Segunda Verção, com uma operção matemática e uma correção de impressão

--- src/gitusedexample/GitUsedExample.java
+++ src/gitusedexample/GitUsedExample.java
@@ -6,7 +6,8 @@ public class GitUsedExample {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        System.out.append("A sua mãe ta sabende que você gosta de rapazes?");
+        System.out.println("A sua mãe ta sabende que você gosta de rapazes?");
+        System.out.println(5 + 3);
     }
     
 }
