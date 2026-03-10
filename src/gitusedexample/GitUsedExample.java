19f3c9bac858099b8c59b69861fada5cc5911d28
NINJA88423 <cesar.smiguel89@gmail.com>
2026-03-09 21:26:53 -0300

    Primeira Verção apenas com impressão

---
+++ src/gitusedexample/GitUsedExample.java
@@ -0,0 +1,12 @@
+package gitusedexample;
+
+
+
+public class GitUsedExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.append("A sua mãe ta sabende que você gosta de rapazes?");
+    }
+    
+}
